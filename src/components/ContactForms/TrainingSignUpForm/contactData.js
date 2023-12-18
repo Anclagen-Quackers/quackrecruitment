eaf28709883450f9b4b54693742d679eda22aa4e
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-12-18 19:50:30 +0100

    signup form draft

---
+++ src/components/ContactForms/TrainingSignUpForm/contactData.js
@@ -0,0 +1,32 @@
+export const initialState = {
+  // Personal Details
+  title: "",
+  "first-name": "",
+  "last-name": "",
+  "date-of-birth": "",
+  "ni-number": "",
+  ethnicity: "",
+  email: "",
+  "mobile-number": "",
+  english: "no",
+  "employment-status": "no",
+  "employment-status-info": "",
+  "criminal-record": "no",
+  "job-seeker": "no",
+
+  // course preferences and eligibility
+  "course-interest": "",
+  location: "",
+  disabilities: "no",
+  "disabilities-info": "",
+  "further-info": "",
+
+  // Honey Pot
+  botInput: "",
+};
+
+export const uploadInitialState = {
+  "proof-of-address": null,
+  "proof-id": null,
+  "proof-of-right-to-work": null,
+};
--- src/components/Formik/FormikYesNo/index.jsx
+++ src/components/Formik/FormikYesNo/index.jsx
@@ -1,7 +1,6 @@
-import { Field, useField, ErrorMessage } from "formik";
-import FormikField from "../FormikFeild"; // Assuming FormikField is in the same directory
+import { Field, useField } from "formik";
 
-function FormikYesNoDetails({ name, question, detailsLabel, placeholder = "" }) {
+function FormikYesNo({ name, question }) {
   const [field] = useField(name);
 
   return (
@@ -9,7 +8,7 @@ function FormikYesNoDetails({ name, question, detailsLabel, placeholder = "" }) 
       <div className="mt-2">
         <span className="me-2">{question}</span>
         <div className="mt-2 flex items-center space-x-3 gap-6">
-          <label className="flex items-center cursor-pointer " tabIndex={"1"}>
+          <label className="flex items-center cursor-pointer">
             <Field type="radio" name={name} value="yes" className="sr-only" />
             <span className={`w-6 h-6 mr-2 rounded-full border-2 ${field.value === "yes" ? "bg-quackred-950 border-white" : "border-white bg-gray-100"}`}></span>
             Yes
@@ -21,13 +20,8 @@ function FormikYesNoDetails({ name, question, detailsLabel, placeholder = "" }) 
           </label>
         </div>
       </div>
-      {field.value === "yes" && detailsLabel && (
-        <div className="mt-1">
-          <FormikField label={detailsLabel} type="text" id={`${name}-info`} name={`${name}-info`} placeholder={placeholder} />
-        </div>
-      )}
     </>
   );
 }
 
-export default FormikYesNoDetails;
+export default FormikYesNo;
